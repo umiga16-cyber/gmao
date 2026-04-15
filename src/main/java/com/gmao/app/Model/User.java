2dc843344011a600685db59b4f3c96473c2ebccd
umiga16-cyber <umiga16@gmail.com>
2026-04-15 16:13:31 +0100

    fixaxion + backend

--- src/main/java/com/gmao/app/Model/User.java
+++ src/main/java/com/gmao/app/Model/User.java
@@ -21,7 +21,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long userId;
+    private Long id;
     
     @Column(name = "nom", nullable = false)
     @NotBlank(message = "El nombre es obligatorio")
@@ -53,8 +53,8 @@ public class User {
         this.password = password;
     }
     
-    public Long getUserId() { return userId; }
-    public void setUserId(Long userId) { this.userId = userId; }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
     
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
